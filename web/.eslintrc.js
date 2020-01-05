4ed10339f923326f740726a1b19c7eef6b3c1e2d
Samuel Monteiro <samu.mjs@gmail.com>
2020-01-04 23:55:14 -0300

    Utilizando unform

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'react/jsx-props-no-spreading': 'off',
+    'no-console': 'off',
   },
   settings:{
     'import/resolver':{
