4753713a0ad94cc560d982210f6a040512e34246
Samuel Monteiro <samu.mjs@gmail.com>
2020-02-14 12:38:11 -0300

    input e button

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 // import { Container } from './styles';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite seu nome"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
